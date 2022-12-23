1f1da8f55efea6ec5aea1ea272c006c5ed7f56d0
Senya- Man <masenyagift0@gmail.com>
2022-12-23 10:58:40 +0200

    Added saveDepartment api

---
+++ department-service/src/main/java/net/javaguides/departmentservice/service/DepartmentService.java
@@ -0,0 +1,7 @@
+package net.javaguides.departmentservice.service;
+
+import net.javaguides.departmentservice.dto.DepartmentDto;
+
+public interface DepartmentService {
+    DepartmentDto saveDepartment(DepartmentDto departmentDto);
+}
---
+++ department-service/src/main/java/net/javaguides/departmentservice/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,36 @@
+package net.javaguides.departmentservice.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.departmentservice.dto.DepartmentDto;
+import net.javaguides.departmentservice.entity.Department;
+import net.javaguides.departmentservice.repository.DepartmentRepository;
+import net.javaguides.departmentservice.service.DepartmentService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class DepartmentServiceImpl implements DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public DepartmentDto saveDepartment(DepartmentDto departmentDto) {
+
+        Department department = new Department(
+                departmentDto.getId(),
+                departmentDto.getDepartmentName(),
+                departmentDto.getDepartmentDescription(),
+                departmentDto.getDepartmentCode()
+        );
+
+        Department savedDepartment = departmentRepository.save(department);
+
+        return new DepartmentDto(
+                savedDepartment.getId(),
+                savedDepartment.getDepartmentName(),
+                savedDepartment.getDepartmentDescription(),
+                savedDepartment.getDepartmentCode()
+        );
+
+    }
+}
