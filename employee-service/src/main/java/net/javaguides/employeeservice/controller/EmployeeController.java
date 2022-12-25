b488c4c38836d3e2913a42e9882b22fa18863baf
Senya- Man <masenyagift0@gmail.com>
2022-12-25 12:20:36 +0200

    added employee controller

---
+++ employee-service/src/main/java/net/javaguides/employeeservice/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package net.javaguides.employeeservice.controller;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.employeeservice.dto.EmployeeDto;
+import net.javaguides.employeeservice.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/employees")
+@AllArgsConstructor
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @PostMapping
+    public ResponseEntity<EmployeeDto> saveEmployee(@RequestBody EmployeeDto employeeDto) {
+        EmployeeDto savedEmployee = employeeService.saveEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{employeeId}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable Long employeeId) {
+        EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+}
