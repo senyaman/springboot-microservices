5aa661748c0448780792c3721af714a071fa38bb
Senya- Man <masenyagift0@gmail.com>
2022-12-25 10:32:30 +0200

    added Employee Entity and Repository

---
+++ employee-service/src/main/java/net/javaguides/employeeservice/entity/Employee.java
@@ -0,0 +1,26 @@
+package net.javaguides.employeeservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+}
