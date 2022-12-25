69495728cd8b1190053621fd3771378372fce2a1
Senya- Man <masenyagift0@gmail.com>
2022-12-25 12:19:28 +0200

    added employee service interface and implementation

---
+++ employee-service/src/main/java/net/javaguides/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,51 @@
+package net.javaguides.employeeservice.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.employeeservice.dto.EmployeeDto;
+import net.javaguides.employeeservice.entity.Employee;
+import net.javaguides.employeeservice.repository.EmployeeRepository;
+import net.javaguides.employeeservice.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
+
+        Employee employee = new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+
+        Employee savedEmployee = employeeRepository.save(employee);
+
+        return new EmployeeDto(
+                savedEmployee.getId(),
+                savedEmployee.getFirstName(),
+                savedEmployee.getLastName(),
+                savedEmployee.getEmail()
+        );
+
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+
+        Employee employee = employeeRepository.findById(employeeId).get();
+
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+
+    }
+
+}
