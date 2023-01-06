0bb521c5b170ed06717b8903fad7d8def05e50bb
Senya- Man <masenyagift0@gmail.com>
2023-01-06 15:27:25 +0200

    Registered DepartmentService as a Eureka Client

--- department-service/src/main/java/net/javaguides/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/net/javaguides/departmentservice/DepartmentServiceApplication.java
@@ -3,9 +3,11 @@ package net.javaguides.departmentservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
