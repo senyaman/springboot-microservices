d6498f122170efeade9256ef6f321d3b37370780
Senya- Man <masenyagift0@gmail.com>
2023-01-06 16:06:33 +0200

    Registered EmployeeService as a Eureka Client

--- employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
@@ -3,11 +3,13 @@ package net.javaguides.employeeservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
