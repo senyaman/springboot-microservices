d4a741d067728e10e0050fd89843e47558b1df28
Senya- Man <giftmasenya@yahoo.com>
2023-01-14 15:26:25 +0200

    Added Organization Entity and Repository

---
+++ organization-service/src/main/java/net/javaguides/organizationservice/repository/OrganizationRepository.java
@@ -0,0 +1,7 @@
+package net.javaguides.organizationservice.repository;
+
+import net.javaguides.organizationservice.entity.Organization;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+}
