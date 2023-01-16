b79a42f529af934b2cabb5a72412b2e71e542b4d
Senya- Man <giftmasenya@yahoo.com>
2023-01-16 13:51:40 +0200

    Implemented Organization service

---
+++ organization-service/src/main/java/net/javaguides/organizationservice/service/impl/OrganizationServiceImpl.java
@@ -0,0 +1,26 @@
+package net.javaguides.organizationservice.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.organizationservice.dto.OrganizationDto;
+import net.javaguides.organizationservice.entity.Organization;
+import net.javaguides.organizationservice.repository.OrganizationRepository;
+import net.javaguides.organizationservice.service.OrganizationService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class OrganizationServiceImpl implements OrganizationService {
+
+    private OrganizationRepository organizationRepository;
+    private ModelMapper modelMapper;
+
+    @Override
+    public OrganizationDto saveOrganization(OrganizationDto organizationDto) {
+
+        Organization organization = modelMapper.map(organizationDto, Organization.class);
+        Organization saveOrganization = organizationRepository.save(organization);
+
+        return modelMapper.map(saveOrganization, OrganizationDto.class);
+    }
+}
