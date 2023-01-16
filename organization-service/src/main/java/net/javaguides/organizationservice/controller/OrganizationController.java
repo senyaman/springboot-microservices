125cc0b63cd4bbd73b0ff9f389879afbb3454bbb
Senya- Man <giftmasenya@yahoo.com>
2023-01-16 13:53:13 +0200

    Built save Organization REST API

---
+++ organization-service/src/main/java/net/javaguides/organizationservice/controller/OrganizationController.java
@@ -0,0 +1,25 @@
+package net.javaguides.organizationservice.controller;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.organizationservice.dto.OrganizationDto;
+import net.javaguides.organizationservice.service.OrganizationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/organizations")
+@AllArgsConstructor
+public class OrganizationController {
+
+    private OrganizationService organizationService;
+
+    @PostMapping
+    public ResponseEntity<OrganizationDto> saveOrganization(@RequestBody OrganizationDto organizationDto) {
+        OrganizationDto saveOrganization = organizationService.saveOrganization(organizationDto);
+        return new ResponseEntity<>(saveOrganization, HttpStatus.CREATED);
+    }
+}
