7614709f447b077597d711bcc61c1b89aa0af1c6
Senya- Man <giftmasenya@yahoo.com>
2023-01-17 10:41:32 +0200

    Registered organization-service as a Eureka client

--- organization-service/src/main/java/net/javaguides/organizationservice/OrganizationServiceApplication.java
+++ organization-service/src/main/java/net/javaguides/organizationservice/OrganizationServiceApplication.java
@@ -3,9 +3,11 @@ package net.javaguides.organizationservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrganizationServiceApplication {
 
 	public static void main(String[] args) {
