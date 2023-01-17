ea9e0202f3c7960b3de0fdb33524884dd3f8acb6
Senya- Man <giftmasenya@yahoo.com>
2023-01-17 10:32:06 +0200

    Refactored feign client to use loadbalance

--- employee-service/src/main/java/net/javaguides/employeeservice/feignclient/OrganizationClient.java
+++ employee-service/src/main/java/net/javaguides/employeeservice/feignclient/OrganizationClient.java
@@ -5,9 +5,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ORGANIZATION-SERVICE", url = "http://localhost:8083/api/organizations")
+@FeignClient(name = "ORGANIZATION-SERVICE")
 public interface OrganizationClient {
 
-    @GetMapping("/{organization-code}")
+    @GetMapping("api/organizations/{organization-code}")
     OrganizationDto getOrganizationByCode(@PathVariable("organization-code") String organizationCode);
 }
